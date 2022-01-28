064847ff7d81d056c7bc976c82e44220f62d2a0f
Christophe Pagnoux-Vieuxfort <christophe.pagnoux@abbeal.com>
2022-01-28 15:02:15 +0100

    Zsh: Add alias for starting chromium without web security features

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,20 +1,25 @@
 # SSH using kitten
 alias ssh="kitty +kitten ssh"
 
-# Alias for downloading latest stable version of Adminer in public_html
+# Start chromium without web security features
+alias chromium-unsafe="chromium \
+  --disable-web-security \
+  --user-data-dir=/tmp/chromium-unsafe"
+
+# Download latest stable version of Adminer in public_html
 alias get-adminer-latest="wget https://www.adminer.org/latest-en.php \
   -O ~/public_html/adminer-en.php"
 
-# Alias for generating a self-signed SSL certificate for testing purpose
+# Generate a self-signed SSL certificate for testing purpose
 alias generate-ssl-cert="openssl req -new -x509 -nodes \
   -out ~/Documents/server.crt \
   -keyout ~/Documents/server.key"
 
-# Aliases for controlling Quake Live awesomium_process
+# Control Quake Live awesomium_process
 alias awesomium-stop="killall -STOP awesomium_process.exe"
 alias awesomium-cont="killall -CONT awesomium_process.exe"
 
-# Alias for starting Demopan with TF2 path
+# Start Demopan with TF2 path
 tf2_path="~/.local/share/Steam/steamapps/common/Team\ Fortress\ 2/tf"
 alias demopan-tf2="demopan -w $tf2_path --demos $tf2_path"
 unset tf2_path
